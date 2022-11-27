bafe631bab4627bbfc6874b4b87465860bb14285
Lenovo <d.shparko@mail.ru>
2022-11-27 12:31:40 +0300

    feat: add login page

---
+++ src/Login.js
@@ -0,0 +1,22 @@
+
+
+
+function Login() {
+
+    return (
+        <div className="App">
+
+            <div className="joinChatContainer">
+                <h3>Join a chat</h3>
+                <input
+                    type="text"
+                    placeholder="Username"
+                />
+                <button>Start Chat</button>
+            </div>
+            }
+        </div>
+    );
+}
+
+export default Login;
