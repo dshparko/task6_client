469a275677861921b37c5ecbf51d560b759d253a
Lenovo <d.shparko@mail.ru>
2022-11-27 12:57:24 +0300

    feat: add io

--- src/Login.js
+++ src/Login.js
@@ -1,20 +1,39 @@
+import Messenger from "./Messenger";
+import io from "socket.io-client";
+import {useState} from "react";
+import "./App.css"
 
-
+const socket = io.connect("http://localhost:3001");
 
 function Login() {
+    const [username, setUsername] = useState("");
+    const [showChat, setShowChat] = useState(false);
+
+    const joinRoom = () => {
+        if (username !== "") {
+            socket.emit("join_room");
+            setShowChat(true);
+        }
+    };
 
     return (
         <div className="App">
+            {!showChat ? (
+                <div className="joinChatContainer">
+                    <h3>Join a chat</h3>
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        onChange={(event) => {
+                            setUsername(event.target.value);
+                        }}
+                    />
 
-            <div className="joinChatContainer">
-                <h3>Join a chat</h3>
-                <input
-                    type="text"
-                    placeholder="Username"
-                />
-                <button>Start Chat</button>
-            </div>
-            }
+                    <button onClick={joinRoom}>Start a chat</button>
+                </div>
+            ) : (
+                <Messenger socket={socket} username={username}/>
+            )}
         </div>
     );
 }
