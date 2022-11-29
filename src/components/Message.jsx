2fa7b208186014b2ec3a57bafce0209e1cf9a0e8
Lenovo <d.shparko@mail.ru>
2022-11-29 14:36:09 +0300

    feat: update messanger fuctional

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -4,19 +4,10 @@ const Message = (props) => {
 
     return (
         <div className=' flex flex-col  rounded-lg border-4 p-2 my-4'>
-
-
-    <div className="info">
-
-        <h2 className=' text-black font-bold text-lg'>Sender: {props.sender}, {props.time}</h2>
-        <input id="info__body_1" className="info__switch" type="checkbox"/>
-
-        <label htmlFor="info__body_1" className="info__headline">Theme: {props.title}</label>
-        <div className="info__body">
+            <h2 className=' text-black font-bold text-lg'>Sender: {props.sender}, {props.time}</h2>
+            <h3 className=' text-gray-800 font-semibold text-lg '>Theme: {props.title}</h3>
             <p className=' text-black'>Message: {props.content}</p>
         </div>
-    </div>
-</div>
     );
 };
 
